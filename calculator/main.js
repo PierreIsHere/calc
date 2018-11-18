766f120b80694c6e100caa56003d98b705bd7feb
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2018-11-18 17:12:23 -0500

    Add files via upload

---
+++ calculator/main.js
@@ -0,0 +1,18 @@
+function total() {
+	var equation = document.getElementById("display").value;
+	console.log(equation);
+	if (equation == ""){
+		document.getElementById('display').value = 0;
+	}else{
+		var ans = eval(equation);
+		console.log(ans);
+		document.getElementById('display').value = ans;
+	}
+}
+
+  function backspace() {
+    console.log("backspace")
+    var string  = document.getElementById('display').value;
+    document.getElementById('display').value=string.substring(0,string.length -1);
+  
+  }
