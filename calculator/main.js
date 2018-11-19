74d842e358a63aeb88c3c58164bde35f420d7adc
PierreIsHere <34163077+PierreIsHere@users.noreply.github.com>
2018-11-19 06:43:00 -0500

    Add files via upload

--- calculator/main.js
+++ calculator/main.js
@@ -1,18 +1,24 @@
-function total() {
-	var equation = document.getElementById("display").value;
-	console.log(equation);
-	if (equation == ""){
-		document.getElementById('display').value = 0;
-	}else{
-		var ans = eval(equation);
-		console.log(ans);
-		document.getElementById('display').value = ans;
-	}
-}
+	window.onerror = function(err) {
+    	console.log("syntax error");
+    	document.getElementById("display").value = "Syntax Error";
+    };
 
-  function backspace() {
-    console.log("backspace")
-    var string  = document.getElementById('display').value;
-    document.getElementById('display').value=string.substring(0,string.length -1);
-  
-  }
+	function total() {
+		var equation = document.getElementById("display").value;
+		console.log(equation);
+		if (equation == ""){
+			document.getElementById('display').value = 0;
+		}else{
+			var ans = eval(equation);
+			console.log(ans);
+			document.getElementById('display').value = ans;
+		}
+	}
+
+	  function backspace() {
+	    console.log("backspace")
+	    var string  = document.getElementById('display').value;
+	    document.getElementById('display').value=string.substring(0,string.length -1);
+	  
+	  }
+
